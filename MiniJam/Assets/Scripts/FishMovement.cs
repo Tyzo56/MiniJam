c7f8ad08e83fcb4188a9f4add796193ab06a2b3a
Tyzo56 <serwanttekut@gmail.com>
2022-12-09 17:12:38 +0300

    0.1
    Added water, fish and player (TEST VERSION)

---
+++ MiniJam/Assets/Scripts/FishMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveForward();
+    }
+    void MoveForward()
+    {
+        transform.Translate(Vector3.back * speed * Time.deltaTime);
+    }
+}
