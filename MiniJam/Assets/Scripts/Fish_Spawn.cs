a50bc2dabe6aa7022aada011e6c4113d01aeaec2
loneLONER <varunpoojari2002@gmail.com>
2022-12-10 01:01:29 +0530

    Added varying spawn rates

--- MiniJam/Assets/Scripts/Fish_Spawn.cs
+++ MiniJam/Assets/Scripts/Fish_Spawn.cs
@@ -7,19 +7,27 @@ public class Fish_Spawn : MonoBehaviour
    public Transform[] SpawnPoints;
    public GameObject FishPrefab;
 
-    [SerializeField] float repeatRate;
+    //public float repeatRate;
+    //These 2 varaiables used for varying spawnrates
+    [SerializeField] float minInclusive;
+    [SerializeField] float maxInclusive;
+    public float time;
+    private Vector3 randomSpawnPoint;// used to spawn at random point in x axis
+    
     private void Start()
     {
-        InvokeRepeating("spawnFish", 2f, repeatRate);
+        InvokeRepeating("spawnFish", time, Random.Range(minInclusive, maxInclusive));
     }
 
     private void Update()
     {
-        
+        //repeatRate = Random.Range(0.6f, 5f);
+        randomSpawnPoint = new Vector3(Random.Range(-2, 2), -0.672f, 6.727f);
     }
 
     void spawnFish()
     {
-        Instantiate(FishPrefab, SpawnPoints[0].transform.position, Quaternion.identity);
+        Instantiate(FishPrefab, randomSpawnPoint, Quaternion.identity);
     }
 }
+
